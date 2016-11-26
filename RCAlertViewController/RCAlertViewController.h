b99cb7cab5ad9c859a1208dfd6cffadfd9baf31b
Ryan Cocuzzo <rcocuzzo8@gmail.com>
2016-11-26 12:09:06 -0500

    Initial Commit

---
+++ RCAlertViewController/RCAlertViewController.h
@@ -0,0 +1,19 @@
+//
+//  RCAlertViewController.h
+//  RCAlertViewController
+//
+//  Created by Ryan Cocuzzo on 11/26/16.
+//  Copyright © 2016 rcocuzzo8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RCAlertViewController.
+FOUNDATION_EXPORT double RCAlertViewControllerVersionNumber;
+
+//! Project version string for RCAlertViewController.
+FOUNDATION_EXPORT const unsigned char RCAlertViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCAlertViewController/PublicHeader.h>
+
+
