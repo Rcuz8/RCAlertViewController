5d9cc3df036a78c1f2d369daae637fad4755028a
Ryan Cocuzzo <rcocuzzo8@gmail.com>
2016-11-27 00:56:57 -0500

    Multi-Line subtitles now supported.

--- RCAlertViewController/RCAlertView.swift
+++ RCAlertViewController/RCAlertView.swift
@@ -22,7 +22,7 @@ class RCAlertView: UIViewController {
         self.modalPresentationStyle = .overCurrentContext
         let blur = UIBlurEffect(style: .dark)
         blurView = UIVisualEffectView(effect: blur)
-        blurView.alpha = 0.5
+        blurView.alpha = 0.8
         blurView.frame = self.view.bounds
         self.view.addSubview(blurView)
         
@@ -34,9 +34,9 @@ class RCAlertView: UIViewController {
         adjustDimensions(width: 270, height: 144) //Default Notification Size
         
         self.view.addSubview(rcView)
-        
-        
+   
     }
+    
     
     func addLongPressRecognizer(){
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(dismissMe))
@@ -58,11 +58,24 @@ class RCAlertView: UIViewController {
     
     func dismissMe(){
         navigationController?.popViewController(animated: true)
-        dismiss(animated: true, completion: nil)
+        dismiss(animated: false, completion: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addTitle(title: String, centered: Bool) {
+        self.rcView.addTitle(title: title, centered: centered)
+    }
+    
+    func addSubtitle(title: String, centered: Bool) {
+        self.rcView.addSubtitle(title: title, centered: centered)
+    }
+    
+    func initTextFields(title: String, titleIsCentered: Bool, subTitle: String, subtitleIsCentered: Bool){
+
+     self.rcView.initTextFields(title: title, titleIsCentered: titleIsCentered, subTitle: subTitle, subtitleIsCentered: subtitleIsCentered)
     }
     
     func adjustDimensions(width: Float, height: Float){
@@ -76,8 +89,10 @@ class RCAlertView: UIViewController {
             
             print("Creating frame:\nx: \(CGFloat(margin))\ny: \(CGFloat(marginH))\nwidth: \(CGFloat(width))\nheight: \(CGFloat(height))")
             rcView.frame = CGRect(x:  CGFloat(margin), y: CGFloat(marginH), width: CGFloat(width), height: CGFloat(height))
+            rcView.rectInScreen = CGRect(x:  CGFloat(margin), y: CGFloat(marginH), width: CGFloat(width), height: CGFloat(height))
         } else {
             rcView.frame = CGRect(x:  0, y: 0, width: screenWidth, height: screenHeight)
+            rcView.rectInScreen = CGRect(x:  0, y: 0, width: screenWidth, height: screenHeight)
         }
         
     }
@@ -95,20 +110,14 @@ class RCAlertView: UIViewController {
         }
     }
     
-    /**
-     Lorem ipsum dolor sit amet.
-     
-     @param bar Consectetur adipisicing elit.
-     
-     @return Sed do eiusmod tempor.
-     */
+    
     func changeColor(to: UIColor){
         rcView.backgroundColor = to
     }
     
     func display() {
         let appDelegate = UIApplication.shared.delegate
-        appDelegate?.window??.rootViewController?.present(self, animated: true, completion: nil)
+        appDelegate?.window??.rootViewController?.present(self, animated: false, completion: nil)
     }
     
 }
--- RCAlertViewController/RCCoreView.swift
+++ RCAlertViewController/RCCoreView.swift
@@ -12,9 +12,188 @@ import QuartzCore
 
 class RCCoreView: UIView {
     
+    var rectInScreen: CGRect!
+    
+    var label: UILabel!
+    
+    var subtitle: UILabel!
+    
+    var path: UIBezierPath = UIBezierPath()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        path = UIBezierPath()
         self.layer.cornerRadius = 5
+        self.layer.masksToBounds = true
+        
+        self.setNeedsDisplay()
+        
+        subtitle = UILabel()
+        label = UILabel()
+        subtitle.sizeToFit()
+    
+        
+    }
+    
+   
+    
+    
+    func initTextFields(title: String, titleIsCentered: Bool, subTitle: String, subtitleIsCentered: Bool){
+        // First, lets add the title
+      
+         let cgr = CGRect(x: 5, y: 15, width: frame.width-10, height: 30)
+        label = UILabel(frame: cgr)
+        label.textColor = UIColor.black
+        label.font = UIFont(name: "Avenir", size: 37)
+        
+        label.adjustsFontSizeToFitWidth = true
+        if titleIsCentered {
+            label.textAlignment = .center
+        }
+        label.text = title
+        
+        
+        self.addSubview(label)
+        //Good, now lets add the subtitle
+        
+        if path != nil && label.frame.maxY != nil{
+            
+            let maxTitleY = label.frame.maxY
+            let minLineY = path.bounds.minY
+            print("MAX title Y: \(maxTitleY)")
+            
+            
+            let font = UIFont(name: "HelveticaNeue", size: 18)!
+        
+            
+            let height = subTitle.heightWithConstrainedWidth(width: frame.width-10, font: font)
+            print("Height: \(height)")
+            let subtitleCGR = CGRect(x: 5, y: maxTitleY+10, width: frame.width-10, height: height)
+            subtitle = UILabel(frame: subtitleCGR)
+            print(subtitleCGR)
+            var labell:UILabel = UILabel(frame: subtitleCGR);
+            labell.textAlignment = NSTextAlignment.left;
+            if subtitleIsCentered {
+                labell.textAlignment = .center
+            }
+            
+            
+           // let cgg = CGRect(x: 10, y: 100, width: 300, height: 40)
+            
+            labell.numberOfLines = 0;
+            labell.font = UIFont.systemFont(ofSize: 16.0);
+            labell.preferredMaxLayoutWidth = frame.width-10
+            labell.text = subTitle;
+            labell.lineBreakMode = .byWordWrapping
+            labell.numberOfLines = 0;
+            self.subtitle = labell
+            self.addSubview(labell);
+            
+            let x = self.rectInScreen.minX
+            let y = self.rectInScreen.minY
+            let width = self.rectInScreen.width
+            let newFrame = CGRect(x: x, y: y, width: width, height: subtitleCGR.maxY+80)
+            super.frame = newFrame
+            self.frame = newFrame
+            
+            
+            self.setNeedsDisplay()
+          
+        }
+        
+    }
+    
+    
+    
+   
+    override func draw(_ rect: CGRect) {
+        if subtitle != nil {
+            if subtitle.frame != nil {
+                if subtitle.frame.maxY != nil {
+                    let maxTitleY = label.frame.maxY
+                    
+                    let font = UIFont(name: "HelveticaNeue", size: 18)!
+                    
+                    let height = subtitle.text?.heightWithConstrainedWidth(width: frame.width-10, font: font)
+                    print("Height: \(height)")
+                    let subtitleCGR = CGRect(x: 10, y: maxTitleY+10, width: frame.width-10, height: height!)
+                    
+                    print(subtitle.frame.maxY)
+                    print(subtitleCGR.maxY)
+                    path = UIBezierPath()
+                    path.lineWidth = 2
+                    
+                    let bottomPointOfSubtitle: CGFloat = subtitleCGR.maxY
+                    let lineY: CGFloat = bottomPointOfSubtitle + 5
+                    
+                    UIColor.black.setFill()
+                    UIColor.black.setStroke()
+                
+                    path.move(to: CGPoint(x: 0, y: lineY))
+                    
+                    path.addLine(to: CGPoint(x: frame.maxX, y: lineY))
+                    path.stroke()
+                    path.fill()
+                    
+                    
+                    
+                }
+            }
+        }
+        
+    }
+    
+    
+    
+    func addTitle(title: String, centered: Bool) {
+        print("Title should be added..")
+        let cgr = CGRect(x: 5, y: 15, width: frame.width-10, height: 30)
+        print("Title CGRect: \(cgr)")
+        label = UILabel(frame: cgr)
+        label.textColor = UIColor.black
+        //rcAC.display()
+        label.font = UIFont(name: "Avenir", size: 37)
+        
+        label.adjustsFontSizeToFitWidth = true
+        if centered {
+            label.textAlignment = .center
+        }
+        label.text = title
+        
+        
+        self.addSubview(label)
+        
+        
+        
+    }
+    
+    func addSubtitle(title: String, centered: Bool) {
+        print("Subtitle should be added..")
+        if path != nil && label.frame.maxY != nil{
+            let maxTitleY = label.frame.maxY
+            
+            let minLineY = path.bounds.minY
+            let subtitleCGR = CGRect(x: 5, y: maxTitleY+10, width: frame.width-10, height: 20)
+            //print("Subtitle CGRect: \(subtitleCGR)")
+            //     subtitle.lineBreakMode =
+            subtitle = UILabel(frame: subtitleCGR)
+            subtitle.textColor = UIColor.black
+            //rcAC.display()
+            subtitle.font = UIFont(name: "Avenir", size: 18)
+            
+            subtitle.numberOfLines = 0
+            //  label.adjustsFontSizeToFitWidth = true
+            if centered {
+                subtitle.textAlignment = .center
+            }
+            subtitle.text = title
+            
+            subtitle.preferredMaxLayoutWidth = frame.width - 10
+            subtitle.numberOfLines = 0;
+            subtitle.lineBreakMode = .byCharWrapping
+            
+            self.addSubview(subtitle)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -35,5 +214,13 @@ class RCCoreView: UIView {
     
     func adjustBackgroundColor(to: UIColor){
         self.backgroundColor = to
+    }
+}
+extension String {
+    func heightWithConstrainedWidth(width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        
+        return boundingBox.height
     }
 }
